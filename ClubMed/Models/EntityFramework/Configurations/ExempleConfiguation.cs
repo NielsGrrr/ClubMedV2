984e2b3c3927906f2a1e72aba5332856f19022eb
Alexandre Ostier <alexandre.ostier@etu.univ-smb.fr>
2026-03-11 09:19:11 +0100

    Ajout ExempleConfiguration

---
+++ ClubMed/Models/EntityFramework/Configurations/ExempleConfiguation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ClubMed.Models.EntityFramework.Configurations
+{
+    /*public class ExempleConfiguation : IEntityTypeConfiguration<Exemple>
+    {
+        public void Configure(EntityTypeBuilder<Exemple> builder)
+        {
+            // 1. Nom de la table
+            builder.ToTable("Exemple");
+
+            // 2. Clé primaire
+            builder.HasKey(e => e.Id).HasName("PK_Exemple");
+
+            // 3. Contraintes et propriétés
+            builder.Property(e => e.Name)
+                   .IsRequired()
+                   .HasMaxLength(100)
+                   .HasColumnName("ExempleName")
+                   .HasComment("Le nom de l'exemple");
+
+            builder.HasIndex(e => e.Name)
+                   .IsUnique()
+                   .HasDatabaseName("IX_Exemple_Name");
+
+            // 4. Relations
+            builder.HasOne(e => e.RelatedEntity)
+                   .WithMany(r => r.Exemples)
+                   .HasForeignKey(e => e.RelatedEntityId)
+                   .OnDelete(DeleteBehavior.Cascade)
+                   .HasConstraintName("FK_Exemple_RelatedEntity");
+
+            builder.HasMany(e => e.OtherEntities)
+                   .WithOne(o => o.Exemple)
+                   .HasForeignKey(o => o.ExempleId)
+                   .OnDelete(DeleteBehavior.SetNull)
+                   .HasConstraintName("FK_OtherEntity_Exemple");
+        }
+    }*/
+}
