5023c028e3c6d7a9e4abe807a0f6a3e08463c0be
BazeOnigiri <timkiehl@icloud.com>
2026-03-11 10:16:57 +0100

    model pas fini

---
+++ ClubMed/Models/EntityFramework/Calendrier.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClubMed.Models.EntityFramework
+{
+    [Table("t_e_calendrier_cal")]
+    public class Calendrier
+    {
+        [Key]
+        [Column("cal_date")]
+        [Required]
+        public DateTime CalDate { get; set; }
+    }
+}
