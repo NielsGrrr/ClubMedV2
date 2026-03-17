c9153574741f784e880e8c46bcfbeb63ec2ebd68
mathis-lanfroy <mathis.lanfroy*@etu.univ-smb.fr>
2026-03-17 14:12:50 +0100

    ajout de datarepository

---
+++ ClubMed/Models/DataManager/LocalisationManager.cs
@@ -0,0 +1,48 @@
+﻿using ClubMed.Models.EntityFramework;
+using ClubMed.Models.EntityFramework.Configurations;
+using ClubMed.Models.Repository;
+
+namespace ClubMed.Models.DataManager
+{
+    public class LocalisationManager : IDataRepository<Localisation>
+    {
+        readonly LocalisationConfiguration? localisationDbContext;
+
+        public LocalisationManager() { }
+
+        public LocalisationManager(LocalisationConfiguration context)
+        {
+            localisationDbContext = context;
+        }
+
+        public Task AddAsync(Localisation entity)
+        {
+            return await filmsDbContext.Utilisateurs.ToListAsync();
+        }
+
+        public Task DeleteAsync(Localisation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Localisation>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Localisation?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Localisation?> GetByStringAsync(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Localisation entityToUpdate, Localisation entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
